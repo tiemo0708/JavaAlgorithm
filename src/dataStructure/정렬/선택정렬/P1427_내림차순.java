88e90f96c6ccc639245bb08c1fcef82cb8f7979c
JinHyeon <tiemo0708@naver.com>
2025-10-01 10:04:46 +0900

    선택정렬_P1427_내림차순

---
+++ src/dataStructure/정렬/선택정렬/P1427_내림차순.java
@@ -0,0 +1,33 @@
+package src.dataStructure.정렬.선택정렬;
+
+import java.util.Scanner;
+
+public class P1427_내림차순 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int[] a = new int[str.length()];
+
+        for(int i =0; i<str.length(); i++){
+            a[i] = Integer.parseInt(str.substring(i, i+1));
+        }
+
+        //선택 정렬
+        for(int i =0; i<str.length(); i++){
+            int max =i;
+            for(int j = i+1; j<str.length(); j++){
+                if(a[max]<a[j]){
+                    max = j;
+                }
+            }
+            if(a[max]>a[i]){
+                int temp = a[max];
+                a[max] = a[i];
+                a[i] = temp;
+            }
+        }
+        for(int i =0; i<str.length(); i++){
+            System.out.print(a[i]);
+        }
+    }
+}
