33af91a7325a2fc4b8ffe30c4bc09f3b8004f0fc
JinHyeon <42500530+tiemo0708@users.noreply.github.com>
2026-08-06 13:48:30 +0900

    [D2] Title: 중간 평균값 구하기, Time: 85 ms, Memory: 25,984 KB -BaekjoonHub

---
+++ SWEA/D2/1984. 중간 평균값 구하기/README.md
@@ -0,0 +1,15 @@
+# [D2] 중간 평균값 구하기 - 1984 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Pw_-KAdcDFAUq) 
+
+### 성능 요약
+
+메모리: 25,984 KB, 시간: 85 ms, 코드길이: 883 Bytes
+
+### 제출 일자
+
+2026-08-06 13:48
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int max = 0;
+			int min = 1000;
+			double sum = 0;
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < 10; i++) {
+				int temp =Integer.parseInt(st.nextToken());
+				if(max<temp) {
+					max = temp;
+				}
+				if(min>temp) {
+					min = temp;
+				}
+				sum+=temp;
+			}
+			sum -=(max+min);
+			int result = (int) Math.round(sum/8);
+		
+			sb.append("#").append(test_case).append(" ").append(result).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
