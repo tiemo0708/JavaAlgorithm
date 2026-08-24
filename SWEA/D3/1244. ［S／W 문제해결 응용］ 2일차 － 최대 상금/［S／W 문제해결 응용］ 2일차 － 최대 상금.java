4f8449fd1fa018226cbf1660a6ab50af2464fd9e
JinHyeon <42500530+tiemo0708@users.noreply.github.com>
2026-08-24 22:04:50 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 111 ms, Memory: 56,832 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -4,10 +4,9 @@ import java.util.StringTokenizer;
 
 public class Solution {
 	static int[] arr;
-	static int len; // 자릿수
-	static int N; // 교환 횟수 (자릿수로 캡 걸 것)
+	static int n;
 	static int max;
-
+	static boolean[][] visited;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -15,54 +14,52 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 
 		for (int test_case = 1; test_case <= T; test_case++) {
-
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String str = st.nextToken();
-			len = str.length();
-			arr = new int[len];
-			N = Integer.parseInt(st.nextToken());
-
-			for (int i = 0; i < len; i++) {
-				arr[i] = str.charAt(i) - '0';
+			n = Integer.parseInt(st.nextToken());
+			arr = new int[str.length()];
+			for(int i=0; i<str.length(); i++) {
+				arr[i] = str.charAt(i)-'0';
 			}
-
-			if (N > len) {
-				N = len;
+			
+			if(n>str.length()) {
+				n=str.length();
 			}
-
-			max = 0; 
-			dfs(0, 0);
+			max=0;
+			visited= new boolean[n+1][(int) Math.pow(10, str.length())]; //최대 자릿수
+			dfs(0); //시작점
 			sb.append("#").append(test_case).append(" ").append(max).append("\n");
-
 		}
 		System.out.println(sb);
-	}
 
-	private static void dfs(int start, int depth) {
-		if (depth == N) {
-			int result = 0;
-			for (int i = 0; i < len; i++) {
-				result = result * 10 + arr[i];
-			}
-			max = Math.max(max, result);
+	}
+	private static void dfs(int depth) {
+		int sum = 0;
+		for(int v: arr) {
+			sum = sum*10+v;
+		}
+		if(visited[depth][sum]) {
 			return;
 		}
-
-		for (int i = start; i < len; i++) {
-			for (int j = i+1; j < len; j++) {
+		visited[depth][sum] = true;
+		
+		if(depth==n) {
+			max = Math.max(max, sum);
+			return;
+		}
+		
+		for(int i =0; i<arr.length; i++) {
+			for(int j=i+1; j<arr.length; j++) {
 				swap(i,j);
-				dfs(start, depth + 1);
+				dfs(depth+1);
 				swap(i,j);
 			}
 		}
-
+		
 	}
-
 	private static void swap(int i, int j) {
-		int temp = arr[j];
-		arr[j]=arr[i];
-		arr[i]=temp;
+		int temp = arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
 	}
-	
-
 }
