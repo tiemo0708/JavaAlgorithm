844a6cc3ca9796176411f721e742eabd3ff516be
JinHyeon <42500530+tiemo0708@users.noreply.github.com>
2026-09-04 16:16:11 +0900

    [level 3] Title: 여행경로, Time: 0.62 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,58 +1,50 @@
 import java.util.*;
-
 class Solution {
-	Map<String, List<String>> adjList = new HashMap<>();
-	Map<String, boolean[]> visited = new HashMap<>();
-	List<String> answer;
-
-	public String[] solution(String[][] tickets) {
-
-		// 인접리스트 구성 + 정렬
-		for (String[] ticket : tickets) {
-			String from = ticket[0];
-			String to = ticket[1];
-
-			adjList.computeIfAbsent(from, k -> new ArrayList<>()).add(to);
-
-			for (String key : adjList.keySet()) {
-				Collections.sort(adjList.get(key)); // 정렬
-				visited.put(key, new boolean[adjList.get(key).size()]);
-			}
-
-			List<String> path = new ArrayList<>(); // 방문 공항
-			path.add("ICN");
-			dfs(path, "ICN", tickets.length + 1); // 방문공항누적, 현재위치, 최대개수
-
-		}
-
-		return answer.toArray(new String[0]);
-	}
-
-	private boolean dfs(List<String> path, String cur, int total) {
-		if (path.size() == total) {
-			answer = new ArrayList<>(path);
-			return true;
-		}
-		List<String> next = adjList.get(cur);
-		if (next == null) {
-			return false;
-		}
-		boolean[] used = visited.get(cur);
-		for (int i = 0; i < next.size(); i++) {
-			if (!used[i]) {
-				used[i] = true;
-
-				path.add(next.get(i));
-
-				if (dfs(path, next.get(i), total))
-					return true; // 성공하면 바로 리턴
-
-				path.remove(path.size() - 1);
-
-				used[i] = false;
-			}
-		}
-		return false;
-
-	}
+    Map<String, List<String>> adjList = new HashMap<>(); //공항 인접 리스트
+    Map<String, boolean[]> visited = new HashMap<>(); // 방문 체크
+    String[] answer;
+    int length;
+    
+    public String[] solution(String[][] tickets) {
+        length = tickets.length + 1;
+        // 1. 인접리스트 구성
+        for(String[] ticket : tickets){
+            String from = ticket[0];
+            String to = ticket[1];
+            adjList.computeIfAbsent(from, k -> new ArrayList<>()).add(to);
+        }
+        //2. 알파벳 순으로 정렬
+        for(String key: adjList.keySet()){
+            Collections.sort(adjList.get(key));
+            visited.put(key, new boolean[adjList.get(key).size()]);
+        }
+        
+        // 이동 경로 저장
+        String[] path = new String[tickets.length+1];
+        path[0] = "ICN";
+        dfs(path,1,"ICN"); //경로. 인덱스, 출발지(키)
+        
+        return answer;
+    }
+    private boolean dfs(String[] path, int idx, String cur){
+        if(idx==length){
+            answer = Arrays.copyOf(path,idx);
+            return true;
+        }
+        List<String> next =adjList.get(cur);
+        if(next==null) return false;
+        
+        boolean[] used = visited.get(cur);
+        for(int i=0; i< next.size(); i++){
+            if(!used[i]){
+                used[i] =true; //방문체크
+                path[idx] = next.get(i);
+                if(dfs(path, idx+1, next.get(i))){
+                    return true;
+                }
+                 used[i] =false; //원복
+            }
+        }
+        return false;
+    }
 }
